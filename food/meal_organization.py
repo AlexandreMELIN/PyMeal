0323ebaea7d3df7ed3ba709666049d0b6a7f884e
Alex <alexandre_melin@hotmail.fr>
2025-01-27 20:31:10 +0100

    add calories computation

--- food/meal_organization.py
+++ food/meal_organization.py
@@ -1,20 +1,34 @@
 from abc import abstractmethod
+from functools import reduce
 from typing import List
 
 from food.food import Food
+from food.nutritional_panel import NutritionalPanel, add_two_nutritional_panel
+
 
 class MealItem:
     def __init__(self, food: Food, quantity: float):
         self.food = food
         self.quantity = quantity
 
+    def compute_macro(self) -> NutritionalPanel:
+        ratio = self.quantity / 100
+        return NutritionalPanel(protein=int(self.food.nutritional_panel.protein * ratio),
+                                carbohydrates=int(self.food.nutritional_panel.carbohydrates * ratio),
+                                fat= int(self.food.nutritional_panel.fat * ratio),
+                                fiber= int(self.food.nutritional_panel.fiber * ratio))
+
 
 class Meal:
     def __init__(self, items: List[MealItem]):
         self.items = items
 
+    def compute_macro(self) -> NutritionalPanel:
+        nutritional_panels = map(lambda item: item.compute_macro(), self.items)
+        return reduce(lambda x, y : add_two_nutritional_panel(x, y), nutritional_panels, NutritionalPanel(0,0,0,0))
+
     def __str__(self):
-        return "\t".join([f"{i.food.food_name}: {i.quantity}g\n" for i in self.items])
+        return "\t".join([f"{i.food.food_name}: {i.quantity}g\n" for i in self.items]) + f" {self.compute_macro()}"
 
 class MealOrganization:
     @abstractmethod
@@ -30,8 +44,10 @@ class MealsForADay(MealOrganization):
     def get_meals(self) -> List[Meal]:
         return [self.breakfast, self.lunch, self.dinner]
 
+    def compute_macro(self) -> NutritionalPanel:
+        return reduce(lambda x,y: add_two_nutritional_panel(x,y), map(lambda item: item.compute_macro(), self.get_meals()), NutritionalPanel(0,0,0,0))
     def __str__(self):
-        return "\n\t".join(map(str, self.get_meals()))
+        return "\n\t".join(map(str, self.get_meals())) + f" \n{self.compute_macro()}"
 
 class MealsForAWeek(MealOrganization):
     def __init__(self, monday: MealsForADay, tuesday: MealsForADay, wednesday: MealsForADay, thursday: MealsForADay, friday: MealsForADay, saturday: MealsForADay, sunday: MealsForADay):
--- food/nutritional_panel.py
+++ food/nutritional_panel.py
@@ -12,3 +12,15 @@ class NutritionalPanel:
         self.carbohydrates = carbohydrates
         self.fiber = fiber
         self.fat = fat
+        self.calories = 9 * self.fat + 4 * self.carbohydrates + 4 * self.protein + 2 * self.fiber
+
+    def __str__(self):
+        return f"protein: {self.protein}g, carbs: {self.carbohydrates}g, fat: {self.fat}g, fiber: {self.fiber}g, calories: {self.calories} Kcal"
+
+
+def add_two_nutritional_panel(nutritional_panel1: NutritionalPanel, nutritional_panel2: NutritionalPanel) -> NutritionalPanel:
+    return NutritionalPanel(nutritional_panel1.protein + nutritional_panel2.protein,
+                            nutritional_panel1.carbohydrates + nutritional_panel2.carbohydrates,
+                            nutritional_panel1.fat + nutritional_panel2.fat,
+                            nutritional_panel1.fiber + nutritional_panel2.fiber
+                            )
